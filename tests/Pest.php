56af4d96287a0cd9607b8641f7bcde4b6ebf0e13
AL!R3Z4 <alireza.jazayerei@gmail.com>
2022-10-17 23:31:31 +0330

    Use TestCase for Unit tests too. this enables us to use laravel features e.g container in unit tests.

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,5 +3,5 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
-uses(Tests\TestCase::class)->in('Feature');
+uses(Tests\TestCase::class)->in('Feature', 'Unit');
 uses(RefreshDatabase::class, WithFaker::class)->in('Feature', 'Unit');
